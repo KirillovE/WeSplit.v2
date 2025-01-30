27d61a4fd3f125496d9145ba7b7b4d593a95ec1c
Eugène <Eugene.Kiriloff@iCloud.com>
2025-01-30 11:36:21 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Eugène Kiriloff on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
